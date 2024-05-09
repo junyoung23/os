320337f48841e1f71451206755272f10da4f21d1
junyoung23 <163238619+junyoung23@users.noreply.github.com>
2024-05-09 11:42:32 +0900

    초기화

--- miniOS-main/miniOS-main/kernel/kernel.c
+++ miniOS-main/miniOS-main/kernel/kernel.c
@@ -3,59 +3,60 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include "system.h"
 
+#include "system.h"
 void print_minios(char* str);
 
 int main() {
     print_minios("[MiniOS SSU] Hello, World!");
-
     char *input;
 
     while(1) {
         // readline을 사용하여 입력 받기
         input = readline("커맨드를 입력하세요(종료:exit) : ");
 
-        if (strcmp(input,"exit") == 0) {
+        if (strcmp(input, "exit") == 0) {
             break;
         }
-        
-        if (strcmp(input, "minisystem") == 0) {
+
+        else if (strcmp(input, "minisystem") == 0){
             minisystem();
         }
-     
-        else if (strcmp(input, "up_down_game") == 0) {
-            up_down_game();
-        }
-        
-        else if (strcmp(input, "mini_fork") == 0) {
-            mini_fork();    
-        }
+		
+		else if (strcmp(input, "add") == 0) {
+			char *a, *b;
+			a = readline("첫 번째 값 : ");
+			b = readline("두 번째 값 : ");
+			
+			add(a, b);
+		}
+		
+		else if (strcmp(input, "fork") == 0) {
+			Fork();
+		}
 
-	    else if (strcmp(input, "shm_com") == 0) {
-	        shm_com();
-	    }
+        else if (strcmp(input, "ipc") == 0) {
+			IPC();
+		}
 
-        else if (strcmp(input, "pipe_com") == 0) {
-            pipe_com();
-	    }
-        
+        else if (strcmp(input, "est_pi") == 0) {
+			Est_PI();
+		}
+
         else if (strcmp(input, "rrs") == 0) {
-            RRS();
-        }
+			RRS();
+		}
 
-        else {
-            system(input); 
-        }
+        else system(input);
     }
 
     // 메모리 해제
     free(input);
     print_minios("[MiniOS SSU] MiniOS Shutdown........");
 
-    return (1);
+    return(1);
 }
 
 void print_minios(char* str) {
-        printf("%s\n",str);
+        printf("%s\n", str);
 }
