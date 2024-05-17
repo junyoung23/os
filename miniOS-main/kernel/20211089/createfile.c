f24fcec480907a7ddedcf6b8b0056001f688659b
junyoung23 <163238619+junyoung23@users.noreply.github.com>
2024-05-17 14:14:20 +0900

    갑자기 오류

--- miniOS-main/kernel/20211089/createfile.c
+++ miniOS-main/kernel/20211089/createfile.c
@@ -1,4 +1,8 @@
-#include "createfile.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h> // open 함수가 포함된 헤더 파일
+#include <unistd.h> // close 함수가 포함된 헤더 파일
+#include "system.h"
 
 int createfile(const char *filename) {
     int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
--- miniOS-main/kernel/system.c
+++ miniOS-main/kernel/system.c
@@ -11,13 +11,3 @@ int add(char *x, char *y) {
     printf("%d + %d = %d\n", *x - '0', *y - '0', (*x - '0') + (*y - '0'));
     return 0;
 }
-
-int createfile(const char *filename) {
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
-        perror("Error creating file");
-        return -1;
-    }
-    close(fd);
-    return 0;
-}
