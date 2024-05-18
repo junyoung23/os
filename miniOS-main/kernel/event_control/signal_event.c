988e8fad8842d66b7d9831a00bb93964fb40c742
junyoung23 <163238619+junyoung23@users.noreply.github.com>
2024-05-18 16:25:10 +0900

    쓰레드 합치기

--- miniOS-main/kernel/event_control/signal_event.c
+++ miniOS-main/kernel/event_control/signal_event.c
@@ -1,17 +1,9 @@
 #include "event_control.h"
 #include <stdio.h>
-#include <pthread.h>
 
-void* signal_event_thread(void* arg) {
+void signal_event() {
     pthread_mutex_lock(&mutex);  // 뮤텍스로 보호
     printf("Signaling event...\n");
     pthread_cond_signal(&cond);  // 대기 중인 스레드에게 신호
     pthread_mutex_unlock(&mutex);  // 뮤텍스 잠금 해제
-    return NULL;
-}
-
-void signal_event() {
-    pthread_t tid;
-    pthread_create(&tid, NULL, signal_event_thread, NULL);
-    pthread_detach(tid);  // 메인 스레드와 독립적으로 실행되도록 분리
 }
--- miniOS-main/kernel/event_control/wait_event.c
+++ miniOS-main/kernel/event_control/wait_event.c
@@ -1,18 +1,10 @@
 #include "event_control.h"
 #include <stdio.h>
-#include <pthread.h>
 
-void* wait_for_event_thread(void* arg) {
+void wait_for_event() {
     pthread_mutex_lock(&mutex);  // 뮤텍스로 보호
     printf("Waiting for event...\n");
     pthread_cond_wait(&cond, &mutex);  // 조건 변수를 사용해 대기
     printf("Event signaled.\n");
     pthread_mutex_unlock(&mutex);  // 뮤텍스 잠금 해제
-    return NULL;
-}
-
-void wait_for_event() {
-    pthread_t tid;
-    pthread_create(&tid, NULL, wait_for_event_thread, NULL);
-    pthread_detach(tid);  // 메인 스레드와 독립적으로 실행되도록 분리
 }
