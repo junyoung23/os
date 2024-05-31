3067b9ab54f162be1df901099bab9b805dccaf91
junyoung23 <163238619+junyoung23@users.noreply.github.com>
2024-05-31 15:05:48 +0900

    extern cond mutex

--- miniOS-main/kernel/event_control/signal_event.c
+++ miniOS-main/kernel/event_control/signal_event.c
@@ -1,6 +1,9 @@
 #include "system.h"
 #include <stdio.h>
 
+extern pthread_cond_t cond;
+extern pthread_mutex_t mutex;
+
 void signal_event() {
     pthread_mutex_lock(&mutex);
     pthread_cond_signal(&cond);
