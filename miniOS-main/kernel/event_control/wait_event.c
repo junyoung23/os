bdca43a23e3a82e8b940834872f1789517666000
junyoung23 <163238619+junyoung23@users.noreply.github.com>
2024-05-31 15:04:41 +0900

    undefined mutex, cond

--- miniOS-main/kernel/event_control/wait_event.c
+++ miniOS-main/kernel/event_control/wait_event.c
@@ -1,9 +1,14 @@
 #include "system.h"
 #include <stdio.h>
 
-void signal_event() {
+// extern 선언을 통해 다른 파일에 정의된 변수를 참조
+extern pthread_cond_t cond;
+extern pthread_mutex_t mutex;
+
+void wait_for_event() {
     pthread_mutex_lock(&mutex);
-    pthread_cond_signal(&cond);
+    printf("Waiting for event...\n");
+    pthread_cond_wait(&cond, &mutex);
+    printf("Event signaled.\n");
     pthread_mutex_unlock(&mutex);
-    printf("Event has been signaled.\n");
 }
